da178bef82c7b87a1eea51e33941707d2f13c7f3
vlabson.viana <vlabson.viana@COTECSIS-529073>
2025-09-18 13:16:34 -0300

    feat:subindo uma estrutura organizacional de imports e a introdução de um service course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+
 @NgModule({
   declarations: [
     AppComponent
