91b88af49e4d5dba9199f1432f30480f12bc6620
vlabson <vlabsonsviana@gmail.com>
2025-09-17 23:04:47 -0300

    subindo formatação de table e toobar no modulo cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+
   ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
