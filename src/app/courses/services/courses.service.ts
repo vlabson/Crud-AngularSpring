8019116f3445dc8522313ad6cd9ada544e757050
vlabson <vlabsonsviana@gmail.com>
2025-09-30 21:50:11 -0300

    feat:implementando um loading spinner para melhorar a interatividade com o usuário

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -16,6 +16,8 @@ export class CoursesService {
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      delay(60000),
       tap( courses => console.log(courses))
     );
 
