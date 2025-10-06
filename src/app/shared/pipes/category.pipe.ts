2bfc72749f45dbf82935e4ca41d216d11d5c0846
vlabson <vlabsonsviana@gmail.com>
2025-10-06 19:49:23 -0300

    feat: ajustes componentes e mockup

--- src/app/shared/pipes/category.pipe.ts
+++ src/app/shared/pipes/category.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CategoryPipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): string {
+  transform(value: string): string {
     switch(value){
       case 'front-end' : return 'code';
       case 'back-end' : return 'computer';
